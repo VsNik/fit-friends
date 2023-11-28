8e5c6806fc8c690eadf91ab75419e51d4f84482e
VsNik <V10Nik80@mail.ru>
2023-11-28 18:09:09 +0300

    refactor

--- apps/frontend/src/app/store/auth/async-actions.ts
+++ apps/frontend/src/app/store/auth/async-actions.ts
@@ -4,23 +4,18 @@ import { authApi } from '../../services/auth-api';
 import { history } from '../../utils/history';
 import { RouteName } from '../../constants/route';
 
-export const signupAction = createAsyncThunk<unknown, FormData>(
-  'auth/signup',
-  async (formData) => {
-    if (formData.get('role') === Role.User) {
-      return history.navigate(RouteName.QuestionUser)
-    }
-    return history.navigate(RouteName.QuestionCoach);
+export const signupAction = createAsyncThunk<unknown, FormData>('auth/signup', async (formData) => {
+  if (formData.get('role') === Role.User) {
+    return history.navigate(RouteName.QuestionUser);
   }
-);
+  return history.navigate(RouteName.QuestionCoach);
+});
 
-export const checkAuthAction = createAsyncThunk<IUser>(
-  'auth/check-auth', 
-  async (_, { rejectWithValue }) => {
-    try {
-      const data = await authApi.checkAuth();
-      return data;
-    } catch (err) {
-      return rejectWithValue(err);
-    }
+export const checkAuthAction = createAsyncThunk<IUser>('auth/check-auth', async (_, { rejectWithValue }) => {
+  try {
+    const data = await authApi.checkAuth();
+    return data;
+  } catch (err) {
+    return rejectWithValue(err);
+  }
 });
--- apps/frontend/src/app/store/user/async-actions.ts
+++ apps/frontend/src/app/store/user/async-actions.ts
@@ -3,17 +3,16 @@ import { IUser } from '@fit-friends/shared';
 import { userApi } from '../../services/user-api';
 import { UserInfoType } from '../../types/forms-type';
 
-// Авторизированный пользователь
 export const fetchAuthAction = createAsyncThunk<IUser>('user/fetc-auth', async () => {
   const data = await userApi.fetchAuth();
   return data;
 });
-// Пользователь по ID
+
 export const fetchUserAction = createAsyncThunk<IUser, string>('user/fetch-user', async (id, { rejectWithValue }) => {
   const data = await userApi.fetchUser(id);
   return data;
 });
-// Обновление пользователя
+
 export const updateUserAction = createAsyncThunk<UserInfoType, UserInfoType>('user/update', async (user, { rejectWithValue }) => {
   try {
     const data = await userApi.updateUser(user);
@@ -23,21 +22,20 @@ export const updateUserAction = createAsyncThunk<UserInfoType, UserInfoType>('us
   }
 });
 
-// Загрузить сертификат
 export const addCertificateAction = createAsyncThunk<unknown, { id: string; formData: FormData }>(
   'user/add-certificate',
   async ({ id, formData }) => {
     await userApi.addCertificate(id, formData);
   },
 );
-// Обновить сертификат
+
 export const updateCertificateAction = createAsyncThunk<unknown, { id: string; src: string; formData: FormData }>(
   'user/update-certificate',
   async ({ id, src, formData }) => {
     await userApi.updateCertificate(id, src, formData);
   },
 );
-// Удалить сертификат
+
 export const deleteSertificateAction = createAsyncThunk<unknown, { id: string; src: string }>(
   'user/delete-certificate', 
   async ({ id, src }) => {
